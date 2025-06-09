66100d41076803f98616ed6eb139db39e4e6f52d
Thor Eric Dueholm <thorericdueholm@gmail.com>
2025-06-09 16:20:39 +0200

    Fix public path

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
-const { defineConfig } = require("@vue/cli-service");
+const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-    publicPath: "/gode-penge/",
-    transpileDependencies: true,
-});
+  publicPath: '/Gode-investering/',
+  transpileDependencies: true
+})
